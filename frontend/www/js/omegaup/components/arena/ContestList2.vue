652c9e5e539bedcc59e8a3d8260cdcbf9850b5ce
Michael Serrato <mikebsg01@gmail.com>
2021-05-03 20:06:03 -0500

    Sidebar component - Part 2

--- frontend/www/js/omegaup/components/arena/ContestList2.vue
+++ frontend/www/js/omegaup/components/arena/ContestList2.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
-    <h1>Concursos</h1>
+    <div class="col-sm-12">
+      <h1 class="title">Concursos</h1>
+    </div>
     <b-card no-body>
-      <b-tabs pills card vertical nav-wrapper-class="col-2">
-        <b-tab title="Actuales" title-link-class="text-center" active>
+      <b-tabs
+        v-model="tabIndex"
+        pills
+        card
+        vertical
+        nav-wrapper-class="custom-nav col-2"
+      >
+        <b-tab title="Actuales" :title-link-class="titleLinkClass(0)" active>
           <b-card-text>
             <b-card
               v-for="contest of contests.current"
@@ -16,7 +24,7 @@
             </b-card>
           </b-card-text>
         </b-tab>
-        <b-tab title="Próximos" title-link-class="text-center">
+        <b-tab title="Próximos" :title-link-class="titleLinkClass(1)">
           <b-card-text>
             <b-card
               v-for="contest of contests.future"
@@ -29,7 +37,7 @@
             </b-card>
           </b-card-text>
         </b-tab>
-        <b-tab title="Pasados" title-link-class="text-center">
+        <b-tab title="Pasados" :title-link-class="titleLinkClass(2)">
           <b-card-text>
             <b-card
               v-for="contest of contests.past"
@@ -66,5 +74,33 @@ export default class ArenaContestList extends Vue {
   @Prop() contests!: omegaup.ArenaContests;
   @Prop() isLogged!: boolean;
   T = T;
+  tabIndex = 0;
+
+  titleLinkClass(idx) {
+    if (this.tabIndex === idx) {
+      return ['text-center', 'active-title-link'];
+    } else {
+      return ['text-center', 'title-link'];
+    }
+  }
 }
 </script>
+
+<style type="text/css">
+.title {
+  font-size: 32px;
+  margin-bottom: 30px;
+}
+
+.custom-nav {
+  background-color: #efefef;
+}
+
+.custom-nav .active-title-link {
+  background-color: #5588dd !important;
+}
+
+.custom-nav .title-link {
+  color: #5588dd !important;
+}
+</style>
