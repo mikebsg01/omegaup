a2d66a4ad6bc4e8b55b97b1ea8bf75ac8ae503cb
Michael Serrato <mikebsg01@gmail.com>
2021-05-24 18:23:04 -0500

    tmp #4

--- frontend/www/js/omegaup/components/contest/ContestCard.vue
+++ frontend/www/js/omegaup/components/contest/ContestCard.vue
@@ -1,22 +1,56 @@
 <template>
 	<b-card
-		class="mb-3"
+		class="contest-card mb-3"
 		no-body
 	>
 		<b-card-body>
-			<b-row>
+			<b-row class="mb-4">
 				<b-col cols="9">
-					<b-link :href="`/arena/${contest.alias}`">{{contest.title}}</b-link>
-					<span>{{contest.organizer}}</span>
+					<b-link 
+            :href="`/arena/${contest.alias}`"
+            class="contest-card-title"
+          >
+            {{contest.title}}
+          </b-link>
+					<div class="d-inline-block contest-card-organizer">
+            <img 
+              src="/media/contestlist/organize.svg"
+              class="d-inline-block icon-organize"
+            />
+            <span>
+              {{contest.organizer}}
+            </span>
+          </div>
 				</b-col>
 				<b-col cols="3">
 				</b-col>
 			</b-row>
 			<b-row>
 				<b-col cols="9">
-					<a :href="getTimeLink(contest.start_time.iso())">{{contest.start_time.long()}}</a>
-					<span>{{time.toDDHHMM(contest.duration)}}</span>
-					<span>{{contest.contestants}}</span>
+          <div class="d-inline-block contest-card-date">
+            <img 
+              src="/media/contestlist/schedule.svg"
+              class="d-inline-block icon-schedule"
+            />
+            <span>
+              <b>Termina: </b>
+              <a :href="getTimeLink(contest.finish_time.iso())">{{contest.finish_time | datetime}}</a>
+            </span>
+          </div>
+          <div class="d-inline-block contest-card-duration">
+            <img 
+              src="/media/contestlist/chronometer.svg"
+              class="d-inline-block icon-chronometer"
+            />
+					  <span>{{time.toDDHHMM(contest.duration)}} hrs</span>
+          </div>
+          <div class="d-inline-block contest-card-contestants">
+            <img 
+              src="/media/contestlist/multiple-users.svg"
+              class="d-inline-block icon-multiple-users"
+            />
+					  <span>{{contest.contestants}}</span>
+          </div>
 				</b-col>
 				<b-col cols="3">
 				</b-col>
@@ -31,6 +65,7 @@ import { types } from '../../api_types';
 import T from '../../lang';
 import * as ui from '../../ui';
 import * as time from '../../time';
+import * as moment from 'moment';
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css';
@@ -44,7 +79,16 @@ Vue.use(CardPlugin);
 Vue.use(LinkPlugin);
 Vue.use(LayoutPlugin);
 
-@Component
+@Component({
+  filters: {
+    datetime(dt: Date) {
+      return moment(dt).format('DD/MM/YYYY HH:mm:ss');
+    },
+    duration(seconds: number) {
+      return moment({}).seconds(seconds).format('H:mm');
+    }
+  }
+})
 export default class FilteredList extends Vue {
   @Prop() contest: types.ContestListItem;
   T = T;
@@ -63,4 +107,66 @@ export default class FilteredList extends Vue {
 
 <style lang="scss" scoped>
 @import '../../../../sass/main.scss';
+
+.contest-card {
+  box-shadow: 4px 6px 8px 2px rgba(175, 175, 175, 0.125);
+  border: 1px solid rgba(125, 125, 125, 0.125);
+
+  /deep/ {
+    .contest-card-title {
+      font-size: 22px;
+    }
+
+    .contest-card-organizer {
+      margin-left: 20px;
+
+      & > .icon-organize {
+        height: 20px;
+      }
+
+      & > span {
+        margin-left: 6px;
+        font-size: 16px;
+      }
+    }
+
+    .contest-card-date {
+      & > .icon-schedule {
+        height: 20px;
+      }
+
+      & > span {
+        margin-left: 6px;
+
+        a {
+          color: var(--contest-card-date-link);
+        }
+      }
+    }
+
+    .contest-card-duration {
+      margin-left: 15px;
+
+      & > .icon-chronometer {
+        height: 20px;
+      }
+
+      & > span {
+        margin-left: 6px;
+      }
+    }
+
+    .contest-card-contestants {
+      margin-left: 15px;
+
+      & > .icon-multiple-users {
+        height: 20px;
+      }
+
+      & > span {
+        margin-left: 6px;
+      }
+    }
+  }
+}
 </style>
